55726e07245281439da868eadc7a6da0de455724
AndreasSasDev <directive.athena@gmail.com>
2023-10-23 16:37:51 +0200

    Fix: Colored tags for Kanban lists

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ The tags are applied in the "Settings Tab" of the plugin.
   - Setting to apply a tag color to a Kanban list, when a tag is present in the title.
   - Setting to ommit the `#` from tags in the Kanban view
 
+## Known Issues
+The following is a list of known issues. If you know how to fix them, then all help is welcome.
+- When a Kanban card has more than one Colored Tag, then it isn't always obvious which color is chosen for the background color of that card.
+
 ## Support
 If you like the plugin, and would like to support its development. You can support me on [twitch](https://www.twitch.tv/andreassasdev) or donate on [Ko-fi](https://www.twitch.tv/andreassasdev).
 All donations are welcome, but never feel pressured to give.
--- src/styles/style_wrangler_kanban_titles.ts
+++ src/styles/style_wrangler_kanban_titles.ts
@@ -24,11 +24,16 @@ export class StyleWranglerKanbanTitles extends StyleWrangler {
 		return Object.keys(this.plugin.settings?.customTagColors)
 			.map(tagName => {
 				const color: RGB = this.plugin.settings.customTagColors[tagName];
-				// noinspection CssInvalidFunction
+				// noinspection CssInvalidFunction,CssUnusedSymbol
 				return `
-					div.kanban-plugin__lane:has(a[href="#${tagName}"]){
+					div.kanban-plugin__lane:has(div.kanban-plugin__lane-title-text a[href="#${tagName.toLowerCase()}"]){
 						background : rgba(${color.r}, ${color.g}, ${color.b}, 0.2) !important;
-					}`;
+						border-color: rgba(${color.r}, ${color.g}, ${color.b}, 0.3) !important;
+					}
+					div.kanban-plugin__lane-header-wrapper:has(div.kanban-plugin__lane-title-text a[href="#${tagName.toLowerCase()}"]){
+						border-color: rgba(${color.r}, ${color.g}, ${color.b}, 0.3) !important;
+					}
+				`;
 			}).join('\n');
 	}
 }
