474c44aa338034f8dfacd7620bae6b5111f7bac4
AndreasSasDev <directive.athena@gmail.com>
2023-10-23 17:59:12 +0200

    Fix: Console now prints version
    Version Bump
    
    Fix: Console now prints v
    
    Version Bump
    
    Update internal_version_bump.mjs
    
    Fix: Again
    
    Version Bump
    
    Update internal_version_bump.mjs

--- tools/internal_version_bump.mjs
+++ tools/internal_version_bump.mjs
@@ -8,61 +8,48 @@ import {fileURLToPath} from 'url';
 // ---------------------------------------------------------------------------------------------------------------------
 // Support Code
 // ---------------------------------------------------------------------------------------------------------------------
-function process_file(filepath, new_version=null){
-	fs.readFile(filepath, 'utf8', (err, fileData) => {
-		if (err) {
-			console.error(`Error reading ${filepath}:`, err);
-			process.exit(1);
-		}
-		try {
-			const jsonData = JSON.parse(fileData);
-			if (new_version === null){
-				const versionParts = jsonData.version.split('.').map(Number);
+function process_file(filepath, new_version = null) {
+	try {
+		const fileData = fs.readFileSync(filepath, 'utf8');
+		const jsonData = JSON.parse(fileData);
 
-				switch (versionBumpType) {
-					case 'full':
-						versionParts[0]++; // Increment major version
-						versionParts[1] = 0; // Reset minor version
-						versionParts[2] = 0; // Reset patch version
-						break;
-					case 'change':
-						versionParts[1]++; // Increment minor version
-						versionParts[2] = 0; // Reset patch version
-						break;
-					case 'fix':
-						versionParts[2]++; // Increment patch version
-						break;
-				}
+		if (new_version === null) {
+			const versionParts = jsonData.version.split('.').map(Number);
 
-				new_version = versionParts.join('.');
+			switch (versionBumpType) {
+				case 'full':
+					versionParts[0]++; // Increment major version
+					versionParts[1] = 0; // Reset minor version
+					versionParts[2] = 0; // Reset patch version
+					break;
+				case 'change':
+					versionParts[1]++; // Increment minor version
+					versionParts[2] = 0; // Reset patch version
+					break;
+				case 'fix':
+					versionParts[2]++; // Increment patch version
+					break;
 			}
 
-			jsonData.version = new_version
-
-			fs.writeFile(filepath, JSON.stringify(jsonData, null, 2), (err) => {
-				if (err) {
-					console.error('Error writing package.json:', err);
-					process.exit(1);
-				}
-			});
-
-		} catch (packageParseError) {
-			console.error('Error parsing package.json:', packageParseError);
-			process.exit(1);
+			new_version = versionParts.join('.');
 		}
-	})
-	return new_version
-}
 
+		jsonData.version = new_version;
+
+		fs.writeFileSync(filepath, JSON.stringify(jsonData, null, 2));
+
+		return new_version;
+	} catch (err) {
+		console.error(`Error processing ${filepath}:`, err);
+		process.exit(1);
+	}
+}
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
-const dirname = path.dirname(fileURLToPath(import.meta.url))
-// Define the version bump type ('full', 'change', 'fix')
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 const packageJsonPath = path.join(dirname, '../package.json');
 const manifestJsonPath = path.join(dirname, '../manifest.json');
-
-// Define the version bump type ('full', 'change', 'fix')
 const versionBumpType = process.argv[2];
 
 if (!['full', 'change', 'fix'].includes(versionBumpType)) {
@@ -73,6 +60,6 @@ if (!['full', 'change', 'fix'].includes(versionBumpType)) {
 let new_version = null;
 
 new_version = process_file(packageJsonPath, new_version);
-new_version =process_file(manifestJsonPath, new_version);
+new_version = process_file(manifestJsonPath, new_version);
 
 console.log(`Version bumped to ${new_version}`);
