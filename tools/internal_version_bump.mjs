53be79c74e214c95dd8a49f3a85d7fd082d882d1
Andreas Sas <directive.athena@gmail.com>
2023-10-24 01:45:22 +0200

    Docs: tools

--- tools/internal_version_bump.mjs
+++ tools/internal_version_bump.mjs
@@ -8,15 +8,18 @@ import {fileURLToPath} from 'url';
 // ---------------------------------------------------------------------------------------------------------------------
 // Support Code
 // ---------------------------------------------------------------------------------------------------------------------
-function process_file(filepath, new_version = null) {
+function process_file(filepath, new_version = null, version_bump_type) {
 	try {
+		// Read the content of the JSON file
 		const fileData = fs.readFileSync(filepath, 'utf8');
 		const jsonData = JSON.parse(fileData);
 
+		// If a new version is not provided,
+		// 		determine the new version based on the bump type
 		if (new_version === null) {
 			const versionParts = jsonData.version.split('.').map(Number);
 
-			switch (versionBumpType) {
+			switch (version_bump_type) {
 				case 'full':
 					versionParts[0]++; // Increment major version
 					versionParts[1] = 0; // Reset minor version
@@ -34,8 +37,8 @@ function process_file(filepath, new_version = null) {
 			new_version = versionParts.join('.');
 		}
 
+		// Update the version in the JSON data and write to file
 		jsonData.version = new_version;
-
 		fs.writeFileSync(filepath, JSON.stringify(jsonData, null, 2));
 
 		return new_version;
@@ -59,7 +62,7 @@ if (!['full', 'change', 'fix'].includes(versionBumpType)) {
 
 let new_version = null;
 
-new_version = process_file(packageJsonPath, new_version);
-new_version = process_file(manifestJsonPath, new_version);
+new_version = process_file(packageJsonPath, new_version, versionBumpType);
+new_version = process_file(manifestJsonPath, new_version,versionBumpType);
 
 console.log(`Version bumped to ${new_version}`);
