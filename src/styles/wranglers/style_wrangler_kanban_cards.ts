f8edab0a51f7254890bd6d929e6cc2a1ac141aa9
AndreasSasDev <directive.athena@gmail.com>
2023-10-23 18:39:18 +0200

    Change: const for readability

--- src/styles/wranglers/style_wrangler_kanban_cards.ts
+++ src/styles/wranglers/style_wrangler_kanban_cards.ts
@@ -24,13 +24,18 @@ export class StyleWranglerKanbanCards extends StyleWrangler {
 		return Object.keys(this.plugin.settings?.customTagColors)
 			.map(tagName => {
 				const color: RGB = this.plugin.settings.customTagColors[tagName];
+
+				const rgb:string = `${color.r}, ${color.g}, ${color.b}`;
+				const opacity_background:string = this.plugin.settings.kanbanCardBackgroundOpacity.toString();
+				const opacity_border:string = this.plugin.settings.kanbanCardBorderOpacity.toString();
+
 				// noinspection CssInvalidFunction,CssUnusedSymbol
 				return `
 					div.kanban-plugin__item.has-tag-${tagName.toLowerCase()} div.kanban-plugin__item-title-wrapper { 
-						background: rgba(${color.r}, ${color.g}, ${color.b}, ${this.plugin.settings.kanbanCardBackgroundOpacity}) !important;
+						background: rgba(${rgb}, ${opacity_background}) !important;
 					}
 					div.kanban-plugin__item.has-tag-${tagName.toLowerCase()}{ 
-						border-color: rgba(${color.r}, ${color.g}, ${color.b}, ${this.plugin.settings.kanbanCardBorderOpacity}) !important;
+						border-color: rgba(${rgb}, ${opacity_border}) !important;
 					}
 				`;
 			}).join('\n');
--- src/styles/wranglers/style_wrangler_kanban_lists.ts
+++ src/styles/wranglers/style_wrangler_kanban_lists.ts
@@ -24,14 +24,18 @@ export class StyleWranglerKanbanLists extends StyleWrangler {
 		return Object.keys(this.plugin.settings?.customTagColors)
 			.map(tagName => {
 				const color: RGB = this.plugin.settings.customTagColors[tagName];
+				const rgb:string = `${color.r}, ${color.g}, ${color.b}`;
+				const opacity_background:string = this.plugin.settings.kanbanListBackgroundOpacity.toString();
+				const opacity_border:string = this.plugin.settings.kanbanListBorderOpacity.toString();
+
 				// noinspection CssInvalidFunction,CssUnusedSymbol
 				return `
 					div.kanban-plugin__lane:has(div.kanban-plugin__lane-title-text a[href="#${tagName.toLowerCase()}"]){
-						background : rgba(${color.r}, ${color.g}, ${color.b}, ${this.plugin.settings.kanbanListBackgroundOpacity}) !important;
-						border-color: rgba(${color.r}, ${color.g}, ${color.b}, ${this.plugin.settings.kanbanListBorderOpacity}) !important;
+						background : rgba(${rgb}, ${opacity_background}) !important;
+						border-color: rgba(${rgb}, ${opacity_border}) !important;
 					}
 					div.kanban-plugin__lane-header-wrapper:has(div.kanban-plugin__lane-title-text a[href="#${tagName.toLowerCase()}"]){
-						border-color: rgba(${color.r}, ${color.g}, ${color.b}, ${this.plugin.settings.kanbanListBorderOpacity}) !important;
+						border-color: rgba(${rgb}, ${opacity_border}) !important;
 					}
 				`;
 			}).join('\n');
