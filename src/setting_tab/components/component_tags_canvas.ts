f3fa04da5338acefb20fe3006a01e6b81dd25b32
Andreas Sas <directive.athena@gmail.com>
2023-10-24 03:21:45 +0200

    Fix: Slider & precise value setter for debug mode

--- src/setting_tab/components/component_tags_canvas.ts
+++ src/setting_tab/components/component_tags_canvas.ts
@@ -1,7 +1,7 @@
 // ---------------------------------------------------------------------------------------------------------------------
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
-import {PluginSettingTab, Setting}
+import {PluginSettingTab, Setting, SliderComponent, TextComponent}
 	from "obsidian";
 import ColoredTagWranglerPlugin
 	from "src/main";
@@ -10,18 +10,19 @@ import {SettingsTabComponent}
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
-export class ComponentTagsCanvas extends SettingsTabComponent{
+export class ComponentTagsCanvas extends SettingsTabComponent {
 	// -----------------------------------------------------------------------------------------------------------------
 	// Constructor
 	// -----------------------------------------------------------------------------------------------------------------
-	constructor(plugin:ColoredTagWranglerPlugin,settings_tab:PluginSettingTab, containerEL:HTMLElement) {
-		super(plugin,settings_tab,containerEL);
+	constructor(plugin: ColoredTagWranglerPlugin, settings_tab: PluginSettingTab, containerEL: HTMLElement) {
+		super(plugin, settings_tab, containerEL);
 	}
+
 	// -----------------------------------------------------------------------------------------------------------------
 	// methods
 	// -----------------------------------------------------------------------------------------------------------------
 	public create_component(): Setting {
-		return new Setting(this.containerEL)
+		let setting = new Setting(this.containerEL)
 			.setName("Apply tag color to canvas card")
 			.setDesc(`
 				Applies the tag color, of the tag within the canvas's card, to the background color of the canvas card. 
@@ -32,9 +33,53 @@ export class ComponentTagsCanvas extends SettingsTabComponent{
 						.onChange(async state => {
 							this.plugin.settings.enableCanvas = state;
 							await this.plugin.saveSettings();
+							this.settings_tab.display();
 						})
 				}
 			);
+
+		// Only show this setting when debug mode is enabled
+		//		TODO maybe change this later
+		let sliderElement: SliderComponent; // Little work around to make them update together
+		let textElement: TextComponent;
+
+		if (this.plugin.settings.enableDebugSettings) {
+			setting
+				.addSlider(component => {
+					component
+						.setLimits(0, .5, 0.05)
+						.setDisabled(!this.plugin.settings.enableCanvas)
+						.setValue(this.plugin.settings.CanvasCardBackgroundLuminanceOffset)
+						.onChange(async state => {
+							this.plugin.settings.CanvasCardBackgroundLuminanceOffset = state;
+							await this.plugin.saveSettings();
+
+							// Update the text component's value
+							textElement.setValue(String(state));
+						});
+					sliderElement = component;
+					}
+				).addText((text) => {
+					text
+						.setDisabled(!this.plugin.settings.enableCanvas)
+						.setValue(String(this.plugin.settings.CanvasCardBackgroundLuminanceOffset))
+						.onChange(async state => {
+							// Because this is a text component it needs to be cast to a number
+							let state_as_number = Number(state)
+							if (isNaN(state_as_number) || state_as_number === null){
+								state_as_number = 0
+							}
+
+							this.plugin.settings.CanvasCardBackgroundLuminanceOffset = state_as_number;
+							await this.plugin.saveSettings();
+
+							sliderElement.setValue(state_as_number)
+						});
+					textElement = text;
+				});
+		}
+
+		return setting
 	}
 }
 
