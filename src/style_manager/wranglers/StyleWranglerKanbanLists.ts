0caea0590a44592550c30efd002415a8b8fcafb2
AndreasSasDev <directive.athena@gmail.com>
2023-11-11 16:54:35 +0100

    Update StyleWranglerKanbanLists.ts

--- src/style_manager/wranglers/StyleWranglerKanbanLists.ts
+++ src/style_manager/wranglers/StyleWranglerKanbanLists.ts
@@ -6,6 +6,7 @@ import {StyleWrangler}
 import ColoredTagWranglerPlugin
 	from "src/main";
 import {RGB} from "obsidian";
+import {hslToRgb, rgbToHsl} from "../../lib";
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
@@ -20,47 +21,47 @@ export class StyleWranglerKanbanLists extends StyleWrangler {
 	// Methods
 	// -----------------------------------------------------------------------------------------------------------------
 	assemble_css_light(): Array<string> {
-		const opacity_border = this.plugin.settings.Kanban.Values.ListBorderOpacity;
-
 		return this.get_tags()
 			.map(
 				({tag_name, color, background_color,luminance_offset}) => {
+
+					let background_hsl = rgbToHsl(background_color);
+					background_hsl.l -= luminance_offset;
+
 					return this.assemble_css(
 						"body.theme-light",
 						tag_name,
 						color,
-						background_color,
-						luminance_offset,
-						opacity_border,
+						hslToRgb(background_hsl)
 					)
 				});
 	}
 
 	assemble_css_dark(): Array<string> {
-		const opacity_border = this.plugin.settings.Kanban.Values.ListBorderOpacity;
-
 		return this.get_tags()
 			.map(
 				({tag_name, color, background_color,luminance_offset}) => {
+
+					let background_hsl = rgbToHsl(background_color);
+					background_hsl.l -= luminance_offset;
+
 					return this.assemble_css(
 						"body.theme-dark",
 						tag_name,
 						color,
-						background_color,
-						luminance_offset,
-						opacity_border,
+						hslToRgb(background_hsl)
 					)
 				});
 	}
 
-	private assemble_css(theme:string, tag_name:string, color:RGB, background_color:RGB, luminance_offset:number, opacity_border:number){
+	private assemble_css(theme:string, tag_name:string, color:RGB, background_color:RGB){
 		return`
 ${theme} div.kanban-plugin__lane:has(div.kanban-plugin__lane-title-text a[href="#${tag_name}"]){
 	background : rgb(${background_color.r}, ${background_color.g}, ${background_color.b}}) !important;
-	border-color: rgba(${color.r}, ${color.g}, ${color.b}, ${opacity_border}) !important;
+	border-color: rgb(${color.r}, ${color.g}, ${color.b}) !important;
 }
 ${theme} div.kanban-plugin__lane-header-wrapper:has(div.kanban-plugin__lane-title-text a[href="#${tag_name}"]){
-	border-color: rgba(${color.r}, ${color.g}, ${color.b}, ${opacity_border}) !important;
+	border-color: rgb(${color.r}, ${color.g}, ${color.b}) !important;
 }`
 
 	}
