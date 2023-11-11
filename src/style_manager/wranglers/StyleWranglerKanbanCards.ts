f849590ac441c8e11a6e59760839ba3ab2e1ac70
AndreasSasDev <directive.athena@gmail.com>
2023-11-11 16:53:15 +0100

    Update StyleWranglerKanbanCards.ts

--- src/style_manager/wranglers/StyleWranglerKanbanCards.ts
+++ src/style_manager/wranglers/StyleWranglerKanbanCards.ts
@@ -6,7 +6,7 @@ import {StyleWrangler}
 import ColoredTagWranglerPlugin
 	from "src/main";
 import {RGB} from "obsidian";
-import {rgbToHsl} from "../../lib";
+import {hslToRgb, rgbToHsl} from "../../lib";
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
@@ -26,13 +26,15 @@ export class StyleWranglerKanbanCards extends StyleWrangler {
 		return this.get_tags()
 			.map(
 				({tag_name, color, background_color,luminance_offset}) => {
+
+					let background_hsl = rgbToHsl(background_color);
+					background_hsl.l -= luminance_offset;
+
 					return this.assemble_css(
 						"body.theme-light",
 						tag_name,
 						color,
-						background_color,
-						luminance_offset,
-						opacity_border,
+						hslToRgb(background_hsl)
 					)
 				});
 	}
@@ -43,24 +45,26 @@ export class StyleWranglerKanbanCards extends StyleWrangler {
 		return this.get_tags()
 			.map(
 				({tag_name, color, background_color,luminance_offset}) => {
+
+					let background_hsl = rgbToHsl(background_color);
+					background_hsl.l -= luminance_offset;
+
 					return this.assemble_css(
 						"body.theme-dark",
 						tag_name,
 						color,
-						background_color,
-						luminance_offset,
-						opacity_border,
+						hslToRgb(background_hsl)
 					)
 				});
 	}
 
-	private assemble_css(theme:string, tag_name:string, color:RGB, background_color:RGB, luminance_offset:number, opacity_border:number){
+	private assemble_css(theme:string, tag_name:string, color:RGB, background_color:RGB){
 		return`
 ${theme} div.kanban-plugin__item.has-tag-${tag_name} div.kanban-plugin__item-title-wrapper { 
 	background: rgb(${background_color.r}, ${background_color.g}, ${background_color.b}) !important;
 }
 ${theme} div.kanban-plugin__item.has-tag-${tag_name}{ 
-	border-color: rgba(${color.r}, ${color.g}, ${color.b}, ${opacity_border}) !important;
+	border-color: rgb(${color.r}, ${color.g}, ${color.b}) !important;
 }`
 
 	}
