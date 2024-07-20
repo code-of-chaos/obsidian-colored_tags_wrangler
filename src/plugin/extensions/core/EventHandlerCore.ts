ae937c4c544e63ee0ca82821189184a5f2032e25
AndreasSasDev <directive.athena@gmail.com>
2024-07-20 21:31:02 +0200

    Update EventHandlerCore.ts

--- src/plugin/extensions/core/EventHandlerCore.ts
+++ src/plugin/extensions/core/EventHandlerCore.ts
@@ -13,7 +13,7 @@ export class EventHandlerCore extends EventHandlerPopulator{
 	override ActiveLeafChange(_: WorkspaceLeaf): void | Promise<void> {this.processor()}
 	override MetaDataCacheChanged(_: TFile): void | Promise<void> {this.processor()}
 	override EditorChange(_: Editor, __: MarkdownView | MarkdownFileInfo): void | Promise<void> {this.processor()}
-	override async FileOpenMd(file: TFile): Promise<void> {await sleep(500);this.processor()}
+	override async FileOpenMd(_: TFile): Promise<void> {await sleep(500);this.processor()}
 	override CodeMirror(): void | Promise<void> {this.processor()}
 
 	// -----------------------------------------------------------------------------------------------------------------
@@ -22,7 +22,7 @@ export class EventHandlerCore extends EventHandlerPopulator{
 	private processor(){
 		ServiceProvider.tagRecords.getTagsFlat(false)
 			.filter(record => record.properties_note_tags_enabled && !/[^a-z0-9-_]/i.test(record.core_tagText))
-			.forEach(record => {
+			.forEach(_ => {
 				$(".cm-hashtag-end")
 					.filter((_, elem) => /[^a-z0-9-_]/i.test($(elem).text()))
 					.each((_, elem) => {
